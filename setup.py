c1fb7c4e1f480a0418fe69ddc14449dcecf92e3a
th3unkn0n <40244622+th3unkn0n@users.noreply.github.com>
2019-12-03 17:18:24 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/bin/env python3
+# code by : youtube.com/theunknon
+
+"""
+
+you can re run setup.py 
+if you have added some wrong value
+
+"""
+import os, sys
+import configparser
+re="\033[1;31m"
+gr="\033[1;32m"
+cy="\033[1;36m"
+def banner():
+	os.system('clear')
+	print(f"""
+	{re}╔═╗{cy}┌─┐┌┬┐┬ ┬┌─┐
+	{re}╚═╗{cy}├┤  │ │ │├─┘
+	{re}╚═╝{cy}└─┘ ┴ └─┘┴
+	""")
+banner()
+print(gr+"[+] Installing requierments ...")
+os.system('pip3 install telethon')
+os.system("touch config.data")
+cpass = configparser.RawConfigParser()
+cpass.add_section('cred')
+xid = input(gr+"[+] enter api ID : "+re)
+cpass.set('cred', 'id', xid)
+xhash = input(gr+"[+] enter hash ID : "+re)
+cpass.set('cred', 'hash', xhash)
+xphone = input(gr+"[+] enter phone number : "+re)
+cpass.set('cred', 'phone', xphone)
+setup = open('config.data', 'w')
+cpass.write(setup)
+setup.close()
+print(gr+"[+] setup complete !")
+print(gr+"[+] run python3 teleg-scrapr.py")
