50ba42e1ecaf0c3019e92f4adb6618f3f82885b4
th3unkn0n <40244622+th3unkn0n@users.noreply.github.com>
2019-12-04 17:25:08 +0530

    Added 2 new feature
    1 ) changed name of "teleg-scraper.py" to "scraper.py"
    2 ) Added smsbot.py ---- to bulk sms from .csv data
    3 ) Added add2group.py ---- to bulk addd members to group from .csv data

---
+++ scraper.py
@@ -0,0 +1,102 @@
+from telethon.sync import TelegramClient
+from telethon.tl.functions.messages import GetDialogsRequest
+from telethon.tl.types import InputPeerEmpty
+import os, sys
+import configparser
+import csv
+import time
+
+re="\033[1;31m"
+gr="\033[1;32m"
+cy="\033[1;36m"
+
+def banner():
+    print(f"""
+{re}╔╦╗{cy}┌─┐┬  ┌─┐{re}╔═╗  ╔═╗{cy}┌─┐┬─┐┌─┐┌─┐┌─┐┬─┐
+{re} ║ {cy}├┤ │  ├┤ {re}║ ╦  ╚═╗{cy}│  ├┬┘├─┤├─┘├┤ ├┬┘
+{re} ╩ {cy}└─┘┴─┘└─┘{re}╚═╝  ╚═╝{cy}└─┘┴└─┴ ┴┴  └─┘┴└─
+
+            version : 1.0
+        youtube.com/theunknon
+        """)
+
+cpass = configparser.RawConfigParser()
+cpass.read('config.data')
+
+try:
+    api_id = cpass['cred']['id']
+    api_hash = cpass['cred']['hash']
+    phone = cpass['cred']['phone']
+    client = TelegramClient(phone, api_id, api_hash)
+except KeyError:
+    os.system('clear')
+    banner()
+    print(re+"[!] run python3 setup.py first !!\n")
+    sys.exit(1)
+
+client.connect()
+if not client.is_user_authorized():
+    client.send_code_request(phone)
+    os.system('clear')
+    banner()
+    client.sign_in(phone, input(gr+'[+] Enter the code: '+re))
+ 
+os.system('clear')
+banner()
+chats = []
+last_date = None
+chunk_size = 200
+groups=[]
+ 
+result = client(GetDialogsRequest(
+             offset_date=last_date,
+             offset_id=0,
+             offset_peer=InputPeerEmpty(),
+             limit=chunk_size,
+             hash = 0
+         ))
+chats.extend(result.chats)
+ 
+for chat in chats:
+    try:
+        if chat.megagroup== True:
+            groups.append(chat)
+    except:
+        continue
+ 
+print(gr+'[+] Choose a group to scrape members :'+re)
+i=0
+for g in groups:
+    print(gr+'['+cy+str(i)+']' + ' - ' + g.title)
+    i+=1
+ 
+print('')
+g_index = input(gr+"[+] Enter a Number : "+re)
+target_group=groups[int(g_index)]
+ 
+print(gr+'[+] Fetching Members...')
+time.sleep(1)
+all_participants = []
+all_participants = client.get_participants(target_group, aggressive=True)
+ 
+print(gr+'[+] Saving In file...')
+time.sleep(1)
+with open("members.csv","w",encoding='UTF-8') as f:
+    writer = csv.writer(f,delimiter=",",lineterminator="\n")
+    writer.writerow(['username','user id', 'access hash','name','group', 'group id'])
+    for user in all_participants:
+        if user.username:
+            username= user.username
+        else:
+            username= ""
+        if user.first_name:
+            first_name= user.first_name
+        else:
+            first_name= ""
+        if user.last_name:
+            last_name= user.last_name
+        else:
+            last_name= ""
+        name= (first_name + ' ' + last_name).strip()
+        writer.writerow([username,user.id,user.access_hash,name,target_group.title, target_group.id])      
+print(gr+'[+] Members scraped successfully.')
---
+++ smsbot.py
@@ -0,0 +1,99 @@
+#!/bin/env python3
+from telethon.sync import TelegramClient
+from telethon.tl.types import InputPeerUser
+from telethon.errors.rpcerrorlist import PeerFloodError
+import configparser
+import os, sys
+import csv
+import random
+import time
+
+re="\033[1;31m"
+gr="\033[1;32m"
+cy="\033[1;36m"
+SLEEP_TIME = 30
+
+class main():
+
+    def banner():
+        
+        print(f"""
+    {re}╔╦╗{cy}┌─┐┬  ┌─┐{re}╔═╗  ╔═╗{cy}┌─┐┬─┐┌─┐┌─┐┌─┐┬─┐
+    {re} ║ {cy}├┤ │  ├┤ {re}║ ╦  ╚═╗{cy}│  ├┬┘├─┤├─┘├┤ ├┬┘
+    {re} ╩ {cy}└─┘┴─┘└─┘{re}╚═╝  ╚═╝{cy}└─┘┴└─┴ ┴┴  └─┘┴└─
+
+                version : 1.0
+            youtube.com/theunknon
+            """)
+
+    def send_sms():
+        try:
+            cpass = configparser.RawConfigParser()
+            cpass.read('config.data')
+            api_id = cpass['cred']['id']
+            api_hash = cpass['cred']['hash']
+            phone = cpass['cred']['phone']
+        except KeyError:
+            os.system('clear')
+            main.banner()
+            print(re+"[!] run python3 setup.py first !!\n")
+            sys.exit(1)
+
+        client = TelegramClient(phone, api_id, api_hash)
+         
+        client.connect()
+        if not client.is_user_authorized():
+            client.send_code_request(phone)
+            os.system('clear')
+            main.banner()
+            client.sign_in(phone, input(gr+'[+] Enter the code: '+re))
+        
+        os.system('clear')
+        main.banner()
+        input_file = sys.argv[1]
+        users = []
+        with open(input_file, encoding='UTF-8') as f:
+            rows = csv.reader(f,delimiter=",",lineterminator="\n")
+            next(rows, None)
+            for row in rows:
+                user = {}
+                user['username'] = row[0]
+                user['id'] = int(row[1])
+                user['access_hash'] = int(row[2])
+                user['name'] = row[3]
+                users.append(user)
+        print(gr+"[1] send sms by user ID\n[2] send sms by username ")
+        mode = int(input(gr+"Input : "+re))
+         
+        message = input(gr+"[+] Enter Your Message : "+re)
+         
+        for user in users:
+            if mode == 2:
+                if user['username'] == "":
+                    continue
+                receiver = client.get_input_entity(user['username'])
+            elif mode == 1:
+                receiver = InputPeerUser(user['id'],user['access_hash'])
+            else:
+                print(re+"[!] Invalid Mode. Exiting.")
+                client.disconnect()
+                sys.exit()
+            try:
+                print(gr+"[+] Sending Message to:", user['name'])
+                client.send_message(receiver, message.format(user['name']))
+                print(gr+"[+] Waiting {} seconds".format(SLEEP_TIME))
+                time.sleep(SLEEP_TIME)
+            except PeerFloodError:
+                print(re+"[!] Getting Flood Error from telegram. \n[!] Script is stopping now. \n[!] Please try again after some time.")
+                client.disconnect()
+                sys.exit()
+            except Exception as e:
+                print(re+"[!] Error:", e)
+                print(re+"[!] Trying to continue...")
+                continue
+        client.disconnect()
+        print("Done. Message sent to all users.")
+
+
+
+main.send_sms()
