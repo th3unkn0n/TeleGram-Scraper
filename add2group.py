e00f2908fd82c41c28a7b17816c3f59c07941140
th3unkn0n <40244622+th3unkn0n@users.noreply.github.com>
2019-12-04 17:57:03 +0530

    Update add2group.py

--- add2group.py
+++ add2group.py
@@ -100,7 +100,7 @@ n = 0
 for user in users:
     n += 1
     if n % 50 == 0:
-	    sleep(900)
+	    time.sleep(900)
 	    try:
 	        print ("Adding {}".format(user['id']))
 	        if mode == 1:
